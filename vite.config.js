90219657a5a3922fcd947cdc990feec46494346c
morabessoneorg <mora@setandforget.io>
2023-10-05 20:50:09 -0300

    vite new config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   // base: "/bcsf/"
-  headers: {
-    'Content-Security-Policy': "default-src 'none'; worker-src 'self' blob:",
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'none'; worker-src 'self' blob:",
+    },
   },
 })
