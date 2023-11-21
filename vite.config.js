44d6a5b5b488d02c3143db0c1dfe25c313897b69
morabessoneorg <mora@setandforget.io>
2023-11-21 09:22:34 -0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bcsf/",
-  // server: {
-  //   headers: {
-  //     'Content-Security-Policy': "default-src 'none'; worker-src 'self' blob:",
-  //   },
-  // },
+  // base: "/bcsf/",
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'none'; worker-src 'self' blob:",
+    },
+  },
 })
