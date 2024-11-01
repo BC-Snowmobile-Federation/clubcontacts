c540bcca01e7d2853a2096c75020c3b30aabbabc
montvlein <montvlein@gmail.com>
2024-11-01 14:47:46 -0300

    probando sin cabeceras

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   // base: "/bcsf/",
-  server: {
-    headers: {
-      'Content-Security-Policy': "default-src 'none'; worker-src 'self' blob:",
-    },
-  },
+  // server: {
+  //   headers: {
+  //     'Content-Security-Policy': "default-src 'none'; worker-src 'self' blob:",
+  //   },
+  // },
 })
