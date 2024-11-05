959cd2e5bedddfd8b851f75b5c55093567724829
Fabricio Montivero <fabricio@setandforget.io>
2024-11-05 16:22:36 -0300

    se elimino console.log

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,1 @@
 export const APPS_SCRIPT_URL = import.meta.env.VITE_APPSCRIPT_API || 'https://script.google.com/macros/s/AKfycbyIFIDagEQnkF3YrRICwgmhAq6gGycMpEHTF9oXYkpqx0h4uAmaVF46nhI0zHYW9eC-NA/exec'
-console.log(APPS_SCRIPT_URL);
